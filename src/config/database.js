55b1fbb7cb61425ab6e98caa8570d4b5aa743085
Eduardo Souza <eduardosouzv@gmail.com>
2021-08-30 00:12:48 -0300

    fix: default db name

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
   define: {
     timestamps: false,
     underscored: true,
