6ff84a88f2a54fcc3dfb3bffd28dd5919b904bee
Eduardo Souza <eduardosouzv@gmail.com>
2021-09-01 14:26:42 -0300

    fix: right methods domain

--- src/jobs/BattleCreation.js
+++ src/jobs/BattleCreation.js
@@ -1,10 +1,13 @@
+const PokeApiService = require('../services/PokeApiService');
 const WebhookService = require('../services/WebhookService');
 
 module.exports = {
   key: 'BattleCreation',
   async handle({ data }) {
     const webhookService = new WebhookService();
-    const resolvedBattle = await webhookService.resolveBattle(data.createdBattle);
+    const pokeApiService = new PokeApiService();
+
+    const resolvedBattle = await pokeApiService.resolveBattle(data.createdBattle);
 
     await webhookService.sendBattleToWebhook(resolvedBattle);
   },
--- src/services/PokeApiService.js
+++ src/services/PokeApiService.js
@@ -50,6 +50,36 @@ class PokeApiService {
 
     return allBattles;
   }
+
+  async resolveBattle(createdBattle) {
+    try {
+      const pokeApiClient = new PokeApiClient();
+
+      const { id, defiant_name, opponent_name } = createdBattle;
+
+      const [defiant_info, opponent_info] = (
+        await Promise.all([defiant_name, opponent_name].map(pokemon => pokeApiClient.getPokemonInformation(pokemon)))
+      ).map(pokemon => ({
+        name: pokemon.name,
+        abilities: pokemon.abilities,
+        types: pokemon.types,
+      }));
+
+      const winner = Math.round(Math.random()) ? defiant_name : opponent_name;
+
+      await Battle.update({ winner, status: 'FINISHED' }, { where: { id } });
+
+      return {
+        id,
+        winner,
+        defiant_info,
+        opponent_info,
+      };
+    } catch (error) {
+      await Battle.update({ status: 'ERROR' }, { where: { id: createdBattle.id } });
+      this.sendErrorMessage();
+    }
+  }
 }
 
 module.exports = PokeApiService;
